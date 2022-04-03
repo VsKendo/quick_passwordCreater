e295f26ef88cc3c5d5ca0fb90f3cb1734d8b1da4
VsKendo <1059227871@qq.com>
2022-04-03 19:56:00 +0800

    fix some problems

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# passwordCreater
-To avoid weak password，this progam can create  a strong password  randomly.
+# quick_passwordCreater
+It can generate a strong password quickly, as long as it get the  length of the password you want.
+
+😎Additional, the result will be recorded in 'out.txt' file. 
+
+---
+
+😢This project is no longer mantained, because I got a better idea --- build a CLI tool to create password with python! It can:
+
+1. Custom the length of password
+2. Custom the character set of password
+3. more features ...
+
+To access that project, please move to [passwordCreater](https://github.com/VsKendo/passwordCreater) .
+
+# quick start
+
+All you need to do is compile it and run it in the console like this:
+
+```shell
+./pswCreater
+```
+
+and then input a number( assume I input 10):
+
+```shell
+input the lenth of password: 10
+```
+
+Password generated successfully:
+
+```shell
+complete! your password is: @jMt>ycY)M
+
+this result will be written into out.txt 
+please remember to save it, good bye
+```
+
+Last but no least, you can find the result in the 'out.txt' file. 
+
+```
+--------------------------------------
+password created detail
+local date/time: Sun Apr  3 19:33:02 2022
+
+UTC date/time: Sun Apr  3 11:33:02 2022
+
+your password is: 
+"$%o#&#'Nl
+--------------------------------------
+--------------------------------------
+password created detail
+local date/time: Sun Apr  3 19:48:37 2022
+
+UTC date/time: Sun Apr  3 11:48:37 2022
+
+your password is: 
+@jMt>ycY)M
+--------------------------------------
+```
+
--- pswCreater.cpp
+++ pswCreater.cpp
@@ -1,9 +1,13 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <ctime>
+#include<chrono>
+#include <random>
 using namespace std;
+
 auto seed = chrono::high_resolution_clock::now().time_since_epoch().count();
 mt19937 rnd(seed);
-//ascii is form 0 to 127
-//but 
+
+// get the length of password from user
 int input(){
 	int tmp;
 	while(1){
@@ -13,24 +17,22 @@ int input(){
 	}
 	return tmp;
 }
+
 void printTime(){
-	// 基于当前系统的当前日期/时间
    time_t now = time(0);
-   
-   // 把 now 转换为字符串形式
+
    char* dt = ctime(&now);
-   cout << "password created detail：" << endl;
-   cout << "local date/time：" << dt << endl;
+   cout << "password created detail" << endl;
+   cout << "local date/time: " << dt << endl;
  
-   // 把 now 转换为 tm 结构
    tm *gmtm = gmtime(&now);
    dt = asctime(gmtm);
-   cout << "UTC date/time："<< dt << endl;
+   cout << "UTC date/time: "<< dt << endl;
 }
 
 int main(){
 	int n;
-	cout<<"input the lenth of psw: ";
+	cout<<"input the lenth of password: ";
 	while(1){
 		n=input();
 		//range from 5 to 16
@@ -59,12 +61,13 @@ int main(){
 		str[i]=tmp;
 	}
 	str[n]='\0';
-	cout<<"complete! your psw is: "<<str<<"\n"<<endl;
-	cout<<"it will be write into a file in this dir which filename is out.txt "<<endl;
+	cout<<"complete! your password is: "<<str<<"\n"<<endl;
+	cout<<"this result will be written into out.txt "<<endl;
 	cout<<"please remember to save it, good bye"<<endl;
 	freopen("out.txt","a+",stdout);
 	puts("--------------------------------------");
 	printTime();
+    cout<<"your password is: "<<endl;
 	cout<<str<<endl;
 	puts("--------------------------------------");
 	
